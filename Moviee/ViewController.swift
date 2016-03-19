c9d96ef01bc43613e1f97780f1fcf3924338a86b
Luca Peduto <l.peduto@gmail.com>
2016-03-19 09:56:40 +0100

    Initial Commit

---
+++ Moviee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Moviee
+//
+//  Created by Luca Peduto on 19/03/16.
+//  Copyright © 2016 Eloquent Bit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
